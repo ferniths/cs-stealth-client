f55236638efa0e1d1a506aa5c8388a12c94241eb
fern <fern@sanct.dev>
2026-09-23 22:38:34 +0200

    Update offsets for CS2 patch 2026-09-23

--- src/offsets.h
+++ src/offsets.h
@@ -2,19 +2,19 @@
 #include <cstdint>
 
 namespace CLIENT {
-    constexpr std::uintptr_t dwLocalPlayerController = 0x23A78D0;
-    constexpr std::uintptr_t dwLocalPlayerPawn       = 0x23CCC08;
-    constexpr std::uintptr_t dwGameEntitySystem       = 0x2577BE0;
-    constexpr std::uintptr_t dwViewAngles             = 0x23E2C98;
-    constexpr std::uintptr_t dwViewMatrix             = 0x23D21F0;
-    constexpr std::uintptr_t dwHighestEntityIndex     = 0x2090;
+    constexpr std::uintptr_t dwLocalPlayerController = 0x25324D8;
+    constexpr std::uintptr_t dwLocalPlayerPawn       = 0x255B598;
+    constexpr std::uintptr_t dwGameEntitySystem       = 0x2710038;
+    constexpr std::uintptr_t dwViewAngles             = 0x2571108;
+    constexpr std::uintptr_t dwViewMatrix             = 0x25608E0;
+    constexpr std::uintptr_t dwHighestEntityIndex     = 0x2120;
     constexpr std::uintptr_t dwChunkPointers          = 0x10;
     constexpr std::uintptr_t dwSlotStride             = 0x70;
 }
 
 namespace ENGINE2 {
-    constexpr std::uintptr_t dwWindowWidth            = 0x912AC0;
-    constexpr std::uintptr_t dwWindowHeight           = 0x912AC4;
+    constexpr std::uintptr_t dwWindowWidth            = 0x91E4D8;
+    constexpr std::uintptr_t dwWindowHeight           = 0x91E4DC;
 }
 
 namespace SCH {
@@ -26,20 +26,20 @@ namespace SCH {
     constexpr std::uintptr_t m_fFlags                 = 0x3F4;
     constexpr std::uintptr_t m_vecAbsOrigin           = 0xC8;
     constexpr std::uintptr_t m_flDuckAmount           = 0x40C;
-    constexpr std::uintptr_t m_pMovementServices      = 0x1248;
-    constexpr std::uintptr_t m_hOriginalController    = 0x1478;
-    constexpr std::uintptr_t m_entitySpottedState     = 0x1C60;
+    constexpr std::uintptr_t m_pMovementServices      = 0x1330;
+    constexpr std::uintptr_t m_hOriginalController    = 0x1560;
+    constexpr std::uintptr_t m_entitySpottedState     = 0x1E88;
     constexpr std::uintptr_t m_bSpotted               = 0x8;
-    constexpr std::uintptr_t m_hPlayerPawn            = 0x914;
-    constexpr std::uintptr_t m_sSanitizedPlayerName   = 0x868;
-    constexpr std::uintptr_t m_pWeaponServices        = 0x1208;
+    constexpr std::uintptr_t m_hPlayerPawn            = 0x92C;
+    constexpr std::uintptr_t m_sSanitizedPlayerName   = 0x878;
+    constexpr std::uintptr_t m_pWeaponServices        = 0x12F0;
     constexpr std::uintptr_t m_hActiveWeapon          = 0x60;
-    constexpr std::uintptr_t m_AttributeManager       = 0x11A8;
+    constexpr std::uintptr_t m_AttributeManager       = 0x1290;
     constexpr std::uintptr_t m_Item                   = 0x50;
     constexpr std::uintptr_t m_iItemDefinitionIndex   = 0x1BA;
     constexpr std::uintptr_t m_modelState             = 0x140;
-    constexpr std::uintptr_t m_iPing                  = 0x830;
-    constexpr std::uintptr_t m_pCameraServices        = 0x1240;
+    constexpr std::uintptr_t m_iPing                  = 0x838;
+    constexpr std::uintptr_t m_pCameraServices        = 0x1328;
     constexpr std::uintptr_t m_vecCsViewPunchAngle    = 0x48;
     constexpr std::uintptr_t m_nButtons               = 0x50;
 }
